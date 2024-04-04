44e4339b8da5b99e31a7019d59cf16b7d75c9252
Dmitry Aleynik <67788185+dm1tru@users.noreply.github.com>
2024-04-04 18:20:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+
+$conf = parse_ini_file(".env");
+
 echo "<h3>HELLO WORLD!</h3>";
 
 echo "<h2>PHP: " . phpversion() . "</h2>";
 
-$mysqli = new mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'));
+$mysqli = new mysqli($conf['MYSQL_HOST'], $conf['MYSQL_USER'], $conf['MYSQL_PASSWORD']);
 $q = $mysqli->query("SELECT VERSION()");
 $mysql_ver = mysqli_fetch_all($q)[0][0];
 echo "<h2>MYSQL: $mysql_ver</h2>";
