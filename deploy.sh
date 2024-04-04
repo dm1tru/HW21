7e48ae5df9a3123f84ba3fa863aaf4b954d9bdc8
Dmitry Aleynik <67788185+dm1tru@users.noreply.github.com>
2024-04-04 17:56:00 +0300

    add deploy scriptes

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,9 @@
-echo "DEPLOY"
+cp .env.sample .env
+sed -i -- "s|%MYSQL_HOST%|$2|g" .env
+sed -i -- "s|%MYSQL_USER%|$3|g" .env
+sed -i -- "s|%MYSQL_PASSWORD%|$4|g" .env
+sed -i -- "s|%MYSQL_DATABASE%|$5|g" .env
+
+sudo cp deploy/nginx.conf /etc/nginx/sites-enabled/demo.conf
+sudo sed -i -- "s|%DOMAIN%|$1|g" /etc/nginx/sites-enabled/demo.conf
+sudo service nginx restart
